0f48ee93a4238b60ca9ef0cfbf91073446e7e2b7
Arya Izra <aryaizra@webmail.umm.ac.id>
2023-03-25 22:39:56 +0700

    Add planggan class

---
+++ src/Pelanggan.java
@@ -0,0 +1,112 @@
+public class Pelanggan {
+    private String nama;
+    private String alamat;
+    private String noTelp;
+    private String email;
+    private boolean member;
+    private static int noAntrian;
+
+    Pelanggan(String nama, String alamat, String noTelp, String email, boolean member){
+        this.nama = nama;
+        this.alamat = alamat;
+        this.noTelp = noTelp;
+        this.email = email;
+        this.member = member;
+        Pelanggan.noAntrian++;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isMember() {
+        return member;
+    }
+
+    public void setMember(boolean member) {
+        this.member = member;
+    }
+
+    public static int getNoAntrian() {
+        return noAntrian;
+    }
+
+    public static void setNoAntrian(int noAntrian) {
+        Pelanggan.noAntrian = noAntrian;
+    }
+
+    public void registrasi(String nama, String alamat, String noTelp, String email){
+        setNama(nama);
+        setAlamat(alamat);
+        setNoTelp(noTelp);
+        setEmail(email);
+    }
+
+    public double getDiscount(){
+        if (isMember()){
+            return 0.1;
+        }else {
+            return 0.0;
+        }
+    }
+}
+
+class member extends Pelanggan{
+    private int point;
+    member(String nama, String alamat, String noTelp, String email, boolean member) {
+        super(nama, alamat, noTelp, email, member);
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    public void earnPoint(int point) {
+        int currentPoint = getPoint();
+        setPoint(currentPoint + point);
+    }
+    public double getDiscount(){
+        double discount = super.getDiscount();
+        if (isMember() && getPoint() >= 50) {
+            discount += 0.02;
+        }
+        return discount;
+    }
+}
+
+class reguler extends Pelanggan{
+    reguler(String nama, String alamat, String noTelp, String email, boolean member) {
+        super(nama, alamat, noTelp, email, member);
+    }
+}
