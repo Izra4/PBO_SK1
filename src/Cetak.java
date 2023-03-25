24e3e084fbf9b908591bef215f254a9566963daa
izra4 <aryaizra2@gmail.com>
2023-03-26 02:27:34 +0700

    cetakLembar is done?

--- src/Cetak.java
+++ src/Cetak.java
@@ -1,20 +1,63 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Cetak {
+    private int jumlahHalaman;
+    private String jenisPembayaran;
+
+    public Cetak(int jumlahHalaman, String jenisPembayaran) {
+        this.jumlahHalaman = jumlahHalaman;
+        this.jenisPembayaran = jenisPembayaran;
+    }
+    public int getJumlahHalaman() {
+        return jumlahHalaman;
+    }
+    public String getJenisPembayaran() {
+        return jenisPembayaran;
+    }
+    public String getJam(){
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return time.format(formatter);
+    }
 }
 
-class Kertas {
+class cetakLembaran extends Cetak{
+    private String jenisKertas;
+    public cetakLembaran(int jumlahHalaman, String jenisPembayaran, String jenisKertas) {
+        super(jumlahHalaman, jenisPembayaran);
+        this.jenisKertas = jenisKertas;
+    }
+
+    public String getJenisKertas() {
+        return jenisKertas;
+    }
+
+    public void setJenisKertas(String jenisKertas) {
+        this.jenisKertas = jenisKertas;
+    }
+
+    public int hargaCetak(){
+        kertas kertas = new kertas(getJenisKertas());
+        return getJumlahHalaman() * kertas.getHarga();
+    }
+
+
+}
+class kertas {
     public static final String A4 = "A4";
     public static final String A5 = "A5";
     public static final String F4 = "F4";
 
     private final String jenisKertas;
-    private int harga;
+    private final int harga;
 
-    public Kertas(String jenisKertas) {
+    public kertas(String jenisKertas) {
         this.jenisKertas = jenisKertas;
         switch (jenisKertas) {
-            case A4 -> harga = 250;
-            case A5 -> harga = 150;
-            case F4 -> harga = 500;
+            case A4 -> harga = 500;
+            case A5 -> harga = 250;
+            case F4 -> harga = 750;
             default -> harga = 0;
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,61 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
+        Scanner in = new Scanner(System.in);
+        int input,lembar;
+        char req;
+        System.out.println("======== Selamat Datang di Percetakan Filkom ========");
+        do {
+            //Menu Pilihan
+            lembar = 0;
+            UIFunc.tampilan();
+            input = in.nextInt();
+            UIFunc.line();
+            if (input == 1){
+                //will do it later
+                System.out.println("SARI ROTI");
+            } else if (input == 2) {
+                while (true) {
+                    try {
+                        System.out.print("Masukkan berapa lembar yang anda ingin cetak: ");
+                        lembar = in.nextInt();
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Input harus berupa angka");
+                        in.nextLine();
+                    }
+                }
+                in.nextLine();
+                System.out.print("Masukkan jenis pembayaran yang anda suka      : ");
+                String jenisBayar = in.nextLine();
+                System.out.println("Masukkan jenis kertas yang dibutuhkan         :\n1.A4\n2.A5\n3.F4 ");
+                System.out.print("Pilihan Anda: ");
+                int jenisKertas = in.nextInt();
+                String kertas = "";
+                switch (jenisKertas){
+                    case 1 -> kertas = "A4";
+                    case 2 -> kertas = "A5";
+                    case 3 -> kertas = "F4";
+                }
+
+                UIFunc.line();
+                Cetak c1 = new Cetak(lembar,jenisBayar);
+                cetakLembaran l1 = new cetakLembaran(c1.getJumlahHalaman(),c1.getJenisPembayaran(),kertas);
+                System.out.println("Jumlah Harga yang dibayar: "+l1.hargaCetak());
+            } else if (input == 3) {
+                System.out.println("Selamat Tinggal");
+                System.exit(0);
+            }
+            System.out.print("Apakah ada transaksi lain? (y/n): ");
+            req = in.next().charAt(0);
+            UIFunc.line();
+        }while (req != 'n');
+        System.out.println("Selamat Tinggal");
+        System.exit(0);
     }
 }
