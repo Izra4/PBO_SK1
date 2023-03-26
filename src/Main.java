e9ca98cbd417214ce3b60e5869b051f2665289d4
Federico <federicoberto708@gmail.com>
2023-03-26 09:51:53 +0700

    Testing

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,9 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-
-
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Ini rico");
         int input,lembar;
         char req;
         System.out.println("======== Selamat Datang di Percetakan Filkom ========");
